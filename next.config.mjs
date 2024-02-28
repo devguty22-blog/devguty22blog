b4b52caf538e1268eeac320bae997da192e9929f
devguty22 <jmgutierreze@bch.bancodechile.cl>
2024-02-28 17:20:07 -0500

    update process

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  /* config options here */
+  basePath: '/devguty22blog',
+  assetPrefix: '/devguty22blog/',
 }
  
 module.exports = nextConfig
